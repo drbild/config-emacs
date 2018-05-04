73221209387c2bb3ec2297a0f52785a205536b32
David R. Bild <drbild@willbild.com>
2018-05-04 08:08:51 -0500

    enable column-number-mode

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(delete-selection-mode nil)
  '(global-whitespace-mode t)
  '(indent-tabs-mode nil)
