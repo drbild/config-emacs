abdaf7ba6654f642f7ccd39f39ddde715dd8e532
David R. Bild <david.bild@resourcely.io>
2024-08-10 08:26:24 -0500

    emacs: sync settings

--- .emacs.d/settings.el
+++ .emacs.d/settings.el
@@ -7,11 +7,13 @@
  '(delete-selection-mode nil)
  '(global-whitespace-mode t)
  '(indent-tabs-mode nil)
+ '(package-selected-packages
+   '(popper yasnippet-snippets yasnippet add-node-modules-path flymake-eslint company-quickhelp-terminal vterm company-quickhelp eldoc-box treemacs-perspective persp-projectile perspective json-mode asdf typescript-mode yaml-mode treemacs-projectile treemacs rego-mode protobuf-mode prettier nginx-mode markdown-mode ledger-mode kconfig-mode go-mode erlang elixir-mode editorconfig counsel-projectile counsel company cmake-font-lock auto-package-update ace-window diminish))
+ '(package-vc-selected-packages
+   '((vc-use-package :vc-backend Git :url "https://github.com/slotThe/vc-use-package")))
  '(show-paren-mode t)
  '(visible-bell t)
- '(whitespace-style
-   (quote
-    (face trailing empty space-before-tab::tab tab-mark))))
+ '(whitespace-style '(face trailing empty space-before-tab::tab tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
